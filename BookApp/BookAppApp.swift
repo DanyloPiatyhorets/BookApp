06d3099947e8a20c8486aa6256ed7c033bfb7e28
Danylo Piatyhorets <danylo.piatyhorets@thetrainline.com>
2025-07-04 10:15:55 +0100

    Initial Commit

---
+++ BookApp/BookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppApp.swift
+//  BookApp
+//
+//  Created by Danylo Piatyhorets on 04/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BookAppTests/BookAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookAppTests.swift
+//  BookAppTests
+//
+//  Created by Danylo Piatyhorets on 04/07/2025.
+//
+
+import Testing
+@testable import BookApp
+
+struct BookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
